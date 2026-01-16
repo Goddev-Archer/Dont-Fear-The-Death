451541874e63dc47660e8a83f98f95eacbc9b44b
Goddev Archer <santamariachristopher599@gmail.com>
2026-01-16 18:32:35 +0100

    Refactor F11 key press logic into Game_Data class
    Moved the F11 key press simulation logic from Program.cs to a new static method PressF11 in the Game_Data class. Program.cs now calls Game_Data.PressF11 and handles exceptions, improving code organization and separation of concerns.

--- Dont_Fear_The_Death/Game Data.cs
+++ Dont_Fear_The_Death/Game Data.cs
@@ -1,6 +1,84 @@
+using System.Runtime.InteropServices;
+
 namespace Dont_Fear_The_Death;
 
 public class Game_Data
 {
-    
+    public static void PressF11()
+    {
+        INPUT[] inputs = new INPUT[2];
+
+        // Tastendruck F11 (Down)
+        inputs[0].type = INPUT_KEYBOARD;
+        inputs[0].U.ki.wVk = VK_F11;
+        inputs[0].U.ki.wScan = 0;
+        inputs[0].U.ki.dwFlags = 0;
+        inputs[0].U.ki.time = 0;
+        inputs[0].U.ki.dwExtraInfo = IntPtr.Zero;
+
+        // Tastendruck F11 (Up)
+        inputs[1].type = INPUT_KEYBOARD;
+        inputs[1].U.ki.wVk = VK_F11;
+        inputs[1].U.ki.wScan = 0;
+        inputs[1].U.ki.dwFlags = KEYEVENTF_KEYUP;
+        inputs[1].U.ki.time = 0;
+        inputs[1].U.ki.dwExtraInfo = IntPtr.Zero;
+
+        int structSize = Marshal.SizeOf(typeof(INPUT));
+        
+        // Sollte 40 auf x64 und 28 auf x86 sein
+        if (SendInput((uint)inputs.Length, inputs, structSize) == 0)
+            throw new System.ComponentModel.Win32Exception(Marshal.GetLastWin32Error());
+    }
+
+    const ushort VK_F11 = 0x7A;
+    const uint KEYEVENTF_KEYUP = 0x0002;
+    const int INPUT_KEYBOARD = 1;
+
+    [DllImport("user32.dll", SetLastError = true)]
+    static extern uint SendInput(uint nInputs, INPUT[] pInputs, int cbSize);
+
+    [StructLayout(LayoutKind.Sequential)]
+    struct INPUT
+    {
+        public int type;
+        public InputUnion U;
+    }
+
+    [StructLayout(LayoutKind.Explicit)]
+    struct InputUnion
+    {
+        [FieldOffset(0)] public MOUSEINPUT mi;
+        [FieldOffset(0)] public KEYBDINPUT ki;
+        [FieldOffset(0)] public HARDWAREINPUT hi;
+    }
+
+    [StructLayout(LayoutKind.Sequential)]
+    struct KEYBDINPUT
+    {
+        public ushort wVk;
+        public ushort wScan;
+        public uint dwFlags;
+        public uint time;
+        public IntPtr dwExtraInfo;
+    }
+
+    [StructLayout(LayoutKind.Sequential)]
+    struct MOUSEINPUT
+    {
+        public int dx;
+        public int dy;
+        public uint mouseData;
+        public uint dwFlags;
+        public uint time;
+        public IntPtr dwExtraInfo;
+    }
+
+    [StructLayout(LayoutKind.Sequential)]
+    struct HARDWAREINPUT
+    {
+        public uint uMsg;
+        public ushort wParamL;
+        public ushort wParamH;
+    }
 }
--- Dont_Fear_The_Death/Program.cs
+++ Dont_Fear_The_Death/Program.cs
@@ -1,6 +1,6 @@
 ﻿using Terminal.Gui;
 using Terminal.Gui.Views;
-using System.Runtime.InteropServices;
+
 using System.Threading;
 
 namespace Dont_Fear_The_Death;
@@ -9,50 +9,20 @@ class Program
 {
     static void Main(string[] args)
     {
-        PressF11();
-        Console.ReadKey();
-    }
-    
-    static void PressF11()
-    {
-        var inputs = new[]
+        Console.WriteLine("Hello World!");
+        Thread.Sleep(2000); 
+
+        try 
         {
-            new INPUT {
-                type =1, // INPUT_KEYBOARD U = new InputUnion { ki = new KEYBDINPUT { wVk = VK_F11 } }
-            },
-            new INPUT {
-                type =1,
-                U = new InputUnion { ki = new KEYBDINPUT { wVk = VK_F11, dwFlags = KEYEVENTF_KEYUP } }
-            }
-        };
-
-        if (SendInput((uint)inputs.Length, inputs, Marshal.SizeOf<INPUT>()) ==0)
-            throw new System.ComponentModel.Win32Exception(Marshal.GetLastWin32Error());
-    }
-
-    const ushort VK_F11 =0x7A;
-    const uint KEYEVENTF_KEYUP =0x0002;
-
-    [DllImport("user32.dll", SetLastError = true)]
-    static extern uint SendInput(uint nInputs, INPUT[] pInputs, int cbSize);
-
-    [StructLayout(LayoutKind.Sequential)]
-    struct INPUT {
-        public uint type;
-        public InputUnion U;
-    }
-
-    [StructLayout(LayoutKind.Explicit)]
-    struct InputUnion {
-        [FieldOffset(0)] public KEYBDINPUT ki;
-    }
-
-    [StructLayout(LayoutKind.Sequential)]
-    struct KEYBDINPUT {
-        public ushort wVk;
-        public ushort wScan;
-        public uint dwFlags;
-        public uint time;
-        public nuint dwExtraInfo;
+            Game_Data.PressF11();
+            Console.WriteLine("F11 Pressed.");
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"Error: {ex.Message}");
+        }
+        
+        Console.WriteLine("Waiting 10 seconds before exiting...");
+        Thread.Sleep(10000);
     }
 }
