5a7f8cb111fbd1412a25100c331397a2693390ae
Goddev Archer <santamariachristopher599@gmail.com>
2026-01-16 17:45:01 +0100

    Add F11 key simulation and Terminal.Gui package
    Added Terminal.Gui package reference to the project. Implemented F11 key press simulation in Program.cs using Win32 API. Created a placeholder Game_Data class for future game data management.

--- Dont_Fear_The_Death/Program.cs
+++ Dont_Fear_The_Death/Program.cs
@@ -1,9 +1,58 @@
-﻿namespace Dont_Fear_The_Death;
+﻿using Terminal.Gui;
+using Terminal.Gui.Views;
+using System.Runtime.InteropServices;
+using System.Threading;
+
+namespace Dont_Fear_The_Death;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        PressF11();
+        Console.ReadKey();
+    }
+    
+    static void PressF11()
+    {
+        var inputs = new[]
+        {
+            new INPUT {
+                type =1, // INPUT_KEYBOARD U = new InputUnion { ki = new KEYBDINPUT { wVk = VK_F11 } }
+            },
+            new INPUT {
+                type =1,
+                U = new InputUnion { ki = new KEYBDINPUT { wVk = VK_F11, dwFlags = KEYEVENTF_KEYUP } }
+            }
+        };
+
+        if (SendInput((uint)inputs.Length, inputs, Marshal.SizeOf<INPUT>()) ==0)
+            throw new System.ComponentModel.Win32Exception(Marshal.GetLastWin32Error());
+    }
+
+    const ushort VK_F11 =0x7A;
+    const uint KEYEVENTF_KEYUP =0x0002;
+
+    [DllImport("user32.dll", SetLastError = true)]
+    static extern uint SendInput(uint nInputs, INPUT[] pInputs, int cbSize);
+
+    [StructLayout(LayoutKind.Sequential)]
+    struct INPUT {
+        public uint type;
+        public InputUnion U;
+    }
+
+    [StructLayout(LayoutKind.Explicit)]
+    struct InputUnion {
+        [FieldOffset(0)] public KEYBDINPUT ki;
+    }
+
+    [StructLayout(LayoutKind.Sequential)]
+    struct KEYBDINPUT {
+        public ushort wVk;
+        public ushort wScan;
+        public uint dwFlags;
+        public uint time;
+        public nuint dwExtraInfo;
     }
 }
