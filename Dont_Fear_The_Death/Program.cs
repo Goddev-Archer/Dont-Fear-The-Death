a263d6ecc6995c960ec32acedc45099a9e34d46d
Goddev Archer <santamariachristopher599@gmail.com>
2026-01-17 22:53:28 +0100

    Improve user prompt and error handling in Program.cs
    Added a prompt instructing users to click the window for optimal experience and a 5-second delay before simulating the F11 key press. Enhanced error handling with a message about required permissions, and removed unnecessary wait and log statements.

--- Dont_Fear_The_Death/Program.cs
+++ Dont_Fear_The_Death/Program.cs
@@ -14,15 +14,14 @@ class Program
 
         try 
         {
+            Console.WriteLine("For Optimal Experience, click the Window in the next 5 seconds...");
+            Thread.Sleep(5000);
             Game_Data.PressF11();
-            Console.WriteLine("F11 Pressed.");
         }
         catch (Exception ex)
         {
             Console.WriteLine($"Error: {ex.Message}");
+            Console.WriteLine("Make sure to run the application with appropriate permissions. It is POSSIBLE, that the application needs to be run as Administrator to simulate key presses.");
         }
-        
-        Console.WriteLine("Waiting 10 seconds before exiting...");
-        Thread.Sleep(10000);
     }
 }
